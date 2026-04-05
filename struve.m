e65cf4a69d19fd378074e0a534fb1f25827848bb
angelorius <mikhail.torio@gmail.com>
2026-04-05 15:10:59 +0900

    First attempt at fixing convergence issues at z > ~30

--- struve.m
+++ struve.m
@@ -11,7 +11,7 @@ function y = struve(v, z, tol, maxterms)
     halfz = 0.5.*z;
     arg = halfz.*halfz;
     
-    
+
     finalmult = halfz;
     vprod = halffact;
 
@@ -24,15 +24,34 @@ function y = struve(v, z, tol, maxterms)
     % n = 0 term
     dy = 1/(halffact*vprod);
     y = dy;
-    
+
+    if abs(z) < 30
+
     for n = 1:maxterms
         dy = (-dy.*arg)./((n+0.5)*(n+v+0.5));
-    
+        y = y + dy;
+        
         if abs(dy) < tol*abs(y)
             break
         end
+    end
+    else
     
-        y = y + dy;
+        sign = 1;
+        logdy = log(dy);
+        logarg = log(arg);
+    
+        for n = 1:maxterms
+            sign = -sign;
+            logdy = logdy + logarg - log(n+0.5) - log(n+v+0.5);
+            
+            dy = sign*exp(logdy);
+            y = y + dy;
+        
+            if abs(dy) < tol*abs(y)
+                break
+            end
+        end
     end
     
     y = y.*finalmult;
